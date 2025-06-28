4738bee6dc8c9dab18ab556062320cd83c474092
유지수 <jsyu333@gmail.com>
2025-06-28 16:56:06 +0900

    chore: radix-ui 제거

--- src/components/atoms/BaseSelect/BaseSelect.tsx
+++ src/components/atoms/BaseSelect/BaseSelect.tsx
@@ -1,18 +1,7 @@
-import { Select } from '@radix-ui/themes';
 import styles from './BaseSelect.module.css';
 
 function BaseSelect() {
-  return (
-    <>
-      <Select.Root defaultValue="apple">
-        <Select.Trigger />
-        <Select.Content position="popper">
-          <Select.Item value="apple">Apple</Select.Item>
-          <Select.Item value="orange">Orange</Select.Item>
-        </Select.Content>
-      </Select.Root>
-    </>
-  );
+  return <></>;
 }
 
 export default BaseSelect;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Theme } from '@radix-ui/themes';
-import '@radix-ui/themes/styles.css';
 import './styles/font.css';
 import './styles/variables.css';
 import './index.css';
@@ -9,8 +7,6 @@ import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Theme>
-      <App />
-    </Theme>
+    <App />
   </StrictMode>,
 );
