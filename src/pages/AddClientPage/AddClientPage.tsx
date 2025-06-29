5ba1d5473de76829b62f0555fadc60b45415c3e0
유지수 <jsyu333@gmail.com>
2025-06-29 20:32:13 +0900

    chore: 휴식 후 작업 다시 시작

--- src/pages/AddClientPage/AddClientPage.tsx
+++ src/pages/AddClientPage/AddClientPage.tsx
@@ -5,6 +5,7 @@ import ClientList from '@/components/organisms/ClientList';
 import ScrollContainer from '@/components/templates/ScrollContainer';
 import { clientData } from '@/data/clientData';
 import laptopIcon from '@/assets/ic/ic_laptop.svg';
+import Button from '@/components/atoms/Button';
 
 function AddClientPage() {
   return (
@@ -13,9 +14,22 @@ function AddClientPage() {
         <PageTitle text="거래처 등록" iconSrc={laptopIcon} />
       </HeadContent>
       <div className={styles.container}>
-        <ScrollContainer className={styles.clientListCon}>
-          <ClientList clientData={clientData} caption="거래처 등록 현황 표" />
-        </ScrollContainer>
+        <div className={`${styles.containerCon} ${styles.clientListCon}`}>
+          <div className={styles.btnCon}>
+            <Button text="등록" />
+          </div>
+          <ScrollContainer>
+            <ClientList clientData={clientData} caption="거래처 등록 현황 표" />
+          </ScrollContainer>
+        </div>
+        <div className={`${styles.containerCon} ${styles.detailListCon}`}>
+          <div className={styles.btnCon}>
+            <Button text="수정" variant="white" />
+          </div>
+          <ScrollContainer className={styles.clientListCon}>
+            <></>
+          </ScrollContainer>
+        </div>
       </div>
     </>
   );
