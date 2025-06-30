a2994b1e1ff82b0ad82bf29eddbb546d96985feb
유지수 <jsyu333@gmail.com>
2025-06-30 13:35:49 +0900

    feat: 수정모드 전환 작업

--- src/components/organisms/CompanyDetail/CompanyDetail.tsx
+++ src/components/organisms/CompanyDetail/CompanyDetail.tsx
@@ -1,14 +1,20 @@
-import LabelWithRequired from '@/components/atoms/LabelWithRequired';
-import commonTableStyles from '@/styles/table.module.css';
 import styles from './CompanyDetail.module.css';
+import commonTableStyles from '@/styles/table.module.css';
+import Button from '@/components/atoms/Button';
+import LabelWithRequired from '@/components/atoms/LabelWithRequired';
 import type { CompanyType } from '@/types/companyType';
 
 interface CompanyDetailProps {
   caption: string;
   selectedClient: CompanyType | null;
+  isEditMode: boolean;
 }
 
-function CompanyDetail({ caption, selectedClient }: CompanyDetailProps) {
+function CompanyDetail({
+  caption,
+  selectedClient,
+  isEditMode,
+}: CompanyDetailProps) {
   return (
     <table
       className={`${commonTableStyles.commonTable} ${styles.companyTable}`}>
@@ -24,82 +30,248 @@ function CompanyDetail({ caption, selectedClient }: CompanyDetailProps) {
           <th>
             <LabelWithRequired label="사업자등록번호" required />
           </th>
-          <td colSpan={3}>{selectedClient?.brn}</td>
+          <td colSpan={3}>
+            {isEditMode ? (
+              <>
+                <input type="text" defaultValue={selectedClient?.brn} />
+                <Button text="사업자등록상태 조회" variant="white" />
+              </>
+            ) : (
+              selectedClient?.brn
+            )}
+          </td>
         </tr>
         <tr>
           <th>
             <LabelWithRequired label="주민 등록 번호" required />
           </th>
-          <td>{selectedClient?.resident_registration_number}</td>
+          <td>
+            {isEditMode ? (
+              <>
+                <input
+                  type="text"
+                  defaultValue={selectedClient?.resident_registration_number}
+                />
+              </>
+            ) : (
+              selectedClient?.resident_registration_number
+            )}
+          </td>
           <th>
             <LabelWithRequired label="주민기재분" required />
           </th>
-          <td>{selectedClient?.resident_type}</td>
+          <td>
+            <td colSpan={3}>
+              {isEditMode ? (
+                <>{/* radio 컴포넌트 자리 */}</>
+              ) : (
+                selectedClient?.resident_type
+              )}
+            </td>
+          </td>
         </tr>
         <tr>
           <th>
             <LabelWithRequired label="대표자 성명" required />
           </th>
-          <td>{selectedClient?.ceo_name}</td>
+          <td>
+            {isEditMode ? (
+              <>
+                <input type="text" defaultValue={selectedClient?.ceo_name} />
+              </>
+            ) : (
+              selectedClient?.ceo_name
+            )}
+          </td>
           <th>
             <LabelWithRequired label="종 사업장 번호" required />
           </th>
-          <td>{selectedClient?.sub_business_number}</td>
+          <td>
+            {isEditMode ? (
+              <>
+                <input
+                  type="text"
+                  defaultValue={selectedClient?.sub_business_number}
+                />
+              </>
+            ) : (
+              selectedClient?.sub_business_number
+            )}
+          </td>
         </tr>
         <tr>
           <th>
             <LabelWithRequired label="업종" required />
           </th>
-          <td>{selectedClient?.business_type}</td>
+          <td>
+            {isEditMode ? (
+              <>
+                <input
+                  type="text"
+                  defaultValue={selectedClient?.business_type}
+                />
+              </>
+            ) : (
+              selectedClient?.business_type
+            )}
+          </td>
           <th>
             <LabelWithRequired label="종목" required />
           </th>
-          <td>{selectedClient?.item}</td>
+          <td>
+            {isEditMode ? (
+              <>
+                <input type="text" defaultValue={selectedClient?.item} />
+              </>
+            ) : (
+              selectedClient?.item
+            )}
+          </td>
         </tr>
         <tr>
           <th>
             <LabelWithRequired label="주소" required />
           </th>
-          <td colSpan={3}>{selectedClient?.address}</td>
+          <td colSpan={3}>
+            {isEditMode ? (
+              <>
+                <input type="text" defaultValue={selectedClient?.zipcode} />
+                <Button text="우편번호 검색" variant="white" />
+                <input type="text" defaultValue={selectedClient?.address} />
+              </>
+            ) : (
+              <>
+                {`(${selectedClient?.zipcode}) `}
+                {selectedClient?.address}
+              </>
+            )}
+          </td>
         </tr>
         <tr>
           <th>
             <LabelWithRequired label="연락처" />
           </th>
-          <td>{selectedClient?.phone}</td>
+          <td>
+            {isEditMode ? (
+              <>
+                <input type="text" defaultValue={selectedClient?.phone} />
+              </>
+            ) : (
+              selectedClient?.phone
+            )}
+          </td>
           <th>
             <LabelWithRequired label="팩스번호" />
           </th>
-          <td>{selectedClient?.fax}</td>
+          <td>
+            {isEditMode ? (
+              <>
+                <input type="text" defaultValue={selectedClient?.fax} />
+              </>
+            ) : (
+              selectedClient?.fax
+            )}
+          </td>
         </tr>
         <tr>
           <th>
             <LabelWithRequired label="담당(부서)사원" />
           </th>
-          <td colSpan={3}>{selectedClient?.manager}</td>
+          <td colSpan={3}>
+            {isEditMode ? (
+              <>
+                <div>
+                  <span>부서명</span>
+                  <input
+                    type="text"
+                    defaultValue={selectedClient?.department}
+                  />
+                </div>
+                <div>
+                  <span>담당자</span>
+                  <input type="text" defaultValue={selectedClient?.manager} />
+                </div>
+              </>
+            ) : (
+              <>
+                {selectedClient?.department} / {selectedClient?.manager}
+              </>
+            )}
+          </td>
         </tr>
         <tr>
           <th>
             <LabelWithRequired label="인쇄할거래처명" />
           </th>
-          <td colSpan={3}>{selectedClient?.printable_company_name}</td>
+          <td colSpan={3}>
+            {isEditMode ? (
+              <>
+                <input
+                  type="text"
+                  defaultValue={selectedClient?.printable_company_name}
+                />
+              </>
+            ) : (
+              selectedClient?.printable_company_name
+            )}
+          </td>
         </tr>
         <tr>
           <th>
             <LabelWithRequired label="담보설정액" />
           </th>
-          <td>{selectedClient?.guarantee_amount}원</td>
+          <td>
+            {isEditMode ? (
+              <>
+                <input
+                  type="text"
+                  defaultValue={selectedClient?.guarantee_amount}
+                />
+                <span>원</span>
+              </>
+            ) : (
+              selectedClient?.guarantee_amount
+            )}
+          </td>
           <th>
             <LabelWithRequired label="여신한도액" />
           </th>
-          <td>{selectedClient?.credit_limit}원</td>
+          <td>
+            {isEditMode ? (
+              <>
+                <input
+                  type="text"
+                  defaultValue={selectedClient?.credit_limit}
+                />
+                <span>원</span>
+              </>
+            ) : (
+              selectedClient?.credit_limit
+            )}
+          </td>
         </tr>
         <tr>
           <th>
             <LabelWithRequired label="주류코드" />
           </th>
           <td colSpan={3}>
-            {selectedClient?.product_code}호 / {selectedClient?.product_name}
+            {isEditMode ? (
+              <>
+                <input
+                  type="text"
+                  defaultValue={selectedClient?.product_code}
+                />
+                <input
+                  type="text"
+                  defaultValue={selectedClient?.product_name}
+                />
+              </>
+            ) : (
+              <>
+                {selectedClient?.product_code}호 /{' '}
+                {selectedClient?.product_name}
+              </>
+            )}
           </td>
         </tr>
         <tr>
@@ -107,25 +279,69 @@ function CompanyDetail({ caption, selectedClient }: CompanyDetailProps) {
             <LabelWithRequired label="입금 계좌 번호" />
           </th>
           <td colSpan={3}>
-            <p>{selectedClient?.bank}</p>
-            <p>
-              <span>예금주 {selectedClient?.account_holder} / 계좌번호 </span>
-              <span>{selectedClient?.account_number}</span>
-            </p>
+            {isEditMode ? (
+              <>
+                <div>
+                  <span>은행</span>
+                  <input type="text" defaultValue={selectedClient?.bank} />
+                </div>
+                <div>
+                  <span>예금주</span>
+                  <input
+                    type="text"
+                    defaultValue={selectedClient?.account_holder}
+                  />
+                </div>
+                <div>
+                  <span>계좌번호</span>
+                  <input
+                    type="text"
+                    defaultValue={selectedClient?.account_number}
+                  />
+                </div>
+              </>
+            ) : (
+              <>
+                <p>{selectedClient?.bank}</p>
+                <p>
+                  <span>
+                    예금주 {selectedClient?.account_holder} / 계좌번호{' '}
+                  </span>
+                  <span>{selectedClient?.account_number}</span>
+                </p>
+              </>
+            )}
           </td>
         </tr>
         <tr>
           <th>
             <LabelWithRequired label="업체담당자이메일" />
           </th>
-          <td colSpan={3}>{selectedClient?.email}</td>
+          <td colSpan={3}>
+            {isEditMode ? (
+              <>
+                <input type="text" defaultValue={selectedClient?.email} />
+              </>
+            ) : (
+              selectedClient?.email
+            )}
+          </td>
         </tr>
         <tr>
           <th>
             <LabelWithRequired label="거래처 분류명" />
           </th>
           <td colSpan={3}>
-            {selectedClient?.category1} / {selectedClient?.category2}
+            {isEditMode ? (
+              <>
+                <input type="text" defaultValue={selectedClient?.category1} />
+                <input type="text" defaultValue={selectedClient?.category2} />
+              </>
+            ) : (
+              <>
+                {selectedClient?.category1} / {selectedClient?.category2}
+              </>
+            )}
           </td>
         </tr>
         <tr>
@@ -133,20 +349,41 @@ function CompanyDetail({ caption, selectedClient }: CompanyDetailProps) {
             <LabelWithRequired label="거래시작(종료)일" />
           </th>
           <td colSpan={3}>
-            {selectedClient?.contract_start} ~ {selectedClient?.contract_end}
+            {isEditMode ? (
+              <>{/* datepicker 컴포넌트 자리 */}</>
+            ) : (
+              <>
+                {selectedClient?.contract_start} ~{' '}
+                {selectedClient?.contract_end}
+              </>
+            )}
           </td>
         </tr>
         <tr>
           <th>
             <LabelWithRequired label="비고" />
           </th>
-          <td colSpan={3}>{selectedClient?.note || ' '}</td>
+          <td colSpan={3}>
+            {isEditMode ? (
+              <>
+                <input type="text" defaultValue={selectedClient?.note ?? ''} />
+              </>
+            ) : (
+              selectedClient?.note || ' '
+            )}
+          </td>
         </tr>
         <tr>
           <th>
             <LabelWithRequired label="사용여부" />
           </th>
-          <td colSpan={3}>{selectedClient?.is_active}</td>
+          <td colSpan={3}>
+            {isEditMode ? (
+              <>{/* radio 컴포넌트 자리 */}</>
+            ) : (
+              selectedClient?.is_active
+            )}
+          </td>
         </tr>
       </tbody>
     </table>
--- src/components/organisms/CompanyDetailSection/CompanyDetailSection.tsx
+++ src/components/organisms/CompanyDetailSection/CompanyDetailSection.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './CompanyDetailSection.module.css';
 import Button from '@/components/atoms/Button';
 import CompanyDetail from '@/components/organisms/CompanyDetail';
@@ -9,15 +10,40 @@ interface props {
 }
 
 function CompanyDetailSection({ selectedCompany }: props) {
+  // 상세 내영 수정 모드
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  const handleCancel = () => {
+    // 초기 상태로
+    setIsEditMode(false);
+  };
+
+  const handleSave = () => {
+    // 저장 처리
+    setIsEditMode(false);
+  };
+
   return (
     <>
       <div className={styles.btnCon}>
-        <Button text="수정" variant="white" />
+        {isEditMode ? (
+          <>
+            <Button text="취소" variant="white" onClick={handleCancel} />
+            <Button text="저장" variant="black" onClick={handleSave} />
+          </>
+        ) : (
+          <Button
+            text="수정"
+            variant="white"
+            onClick={() => setIsEditMode(true)}
+          />
+        )}
       </div>
       <ScrollContainer>
         <CompanyDetail
           caption="거래처 등록 상세 표"
           selectedClient={selectedCompany}
+          isEditMode={isEditMode}
         />
       </ScrollContainer>
     </>
