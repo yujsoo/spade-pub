1cb0b3d3cc215b671376cd95c7a72ea7ed0ceccb
유지수 <jsyu333@gmail.com>
2025-07-01 15:49:04 +0900

    fix: 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Modal from 'react-modal';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import DefaultLayout from '@/components/templates/DefaultLayout';
 import AddClientPage from '@/pages/AddClientPage/AddClientPage';
 
@@ -9,6 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/addClient" />} />
         <Route element={<DefaultLayout />}>
           <Route path="/addClient" element={<AddClientPage />} />
         </Route>
